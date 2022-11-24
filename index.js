e2c58abf1f19571575c75024f98fa85daaf85679
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2022-11-24 18:23:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const messages = [];
+function writeCards(array, event) {
+    for (let i=0; i < array.length; i++) {
+        const card = `Thank you, ${array[i]}, for the wonderful ${event} gift!`
+        messages.push(card);
+    }
+    return messages;
+
+}
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int);
+        int--;
+    }
+}
+
+console.log(countDown(10));
